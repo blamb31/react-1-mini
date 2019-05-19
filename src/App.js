c548b99a1247c873c093a1540d74f477ed22fcbc
Blake Lamb <blake.lamb31@gmail.com>
2019-05-18 22:36:19 -0600

    Step 1-3 done

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div>
+
+    <span>Picture: </span>
+    <input/>
+
+    <span>Name: </span>
+    <input/>    
+    <button>Add Friend</button>
+
+  </div>
+  );
+}
+
+export default App;
