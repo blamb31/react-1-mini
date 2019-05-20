8b93b1f7583be6ee5a1fd670deb0be3a6bb99ada
Blake Lamb <blake.lamb31@gmail.com>
2019-05-19 22:56:27 -0600

    Finished

--- src/App.js
+++ src/App.js
@@ -1,20 +1,68 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-  <div>
+class App extends Component {
 
-    <span>Picture: </span>
-    <input/>
+  constructor () {
+    super()
+    
+    this.state = {
+      friends: [],
+      picture: '',
+      name: ''
+    }
+  }
 
-    <span>Name: </span>
-    <input/>    
-    <button>Add Friend</button>
+    updatePicture (value) {
+      this.setState({picture: value})
+    }
 
-  </div>
-  );
+    updateName (value) {
+      this.setState({name: value})
+    }
+
+    addFriend() {
+      const {friends, picture, name} = this.state
+
+      let newFriends = friends.slice()
+      newFriends.push({ picture,name })
+
+      this.setState({friends: newFriends, picture : '', name : ''})
+    }
+
+  
+
+  render() {
+
+    const friends = this.state.friends.map((friend, index) => (
+    <div key= {`friend-${index}-${friend.name}`}>
+
+      <img width="100px" src={friend.picture} />
+
+      <span>{friend.name}</span>
+
+    </div>
+
+    ))
+
+    return (
+
+      <div>
+
+        <span>Picture: </span>
+        <input onChange={ (event) => this.updatePicture(event.target.value)} value = {this.state.picture} />
+
+        <span>Name: </span>
+        <input onChange={event => this.updateName(event.target.value)} value = {this.state.name} />    
+        
+        <button onClick={ () => this.addFriend() } >Add Friend</button>
+
+        {friends}
+
+      </div>
+    );
+  }
 }
 
 export default App;
